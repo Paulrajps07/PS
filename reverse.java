95fced608118b3d7789c6d02860dd2b329a97a8b
PAULRAJ P <paulrajps07@gmail.com>
2017-06-29 10:01:02 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Prime_not {
+
+
+	public static void main(String[] args) {
+		Scanner main=new Scanner(System.in);
+		System.out.println("Input : ");
+		String m=main.next();
+		StringBuffer sb=new StringBuffer(m);  
+		sb.reverse();  
+System.out.println("Output :"+sb);
+	}
+
+}
