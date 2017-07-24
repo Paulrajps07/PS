736db896ebef645dbaf07685629989c4762c9295
PAULRAJ P <paulrajps07@gmail.com>
2017-07-24 15:21:33 +0530

    Add files via upload

---
+++ powerDigit.java
@@ -0,0 +1,21 @@
+package ps;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class SAample 
+{
+
+	public static void main(String[] args)
+	{
+
+	Scanner main=new Scanner(System.in);	
+	System.out.println("input : ");
+	int n=main.nextInt();
+	int result=n^n;
+	System.out.println("ouput : "+result);
+	}
+	
+}
+	
+
+
+
